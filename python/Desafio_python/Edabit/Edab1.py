943ee4a86a5421b219dc9eee15dacff5da49a5be
magnusthered471 <ainzooalgown471@gmail.com>
2026-07-02 10:29:29 -0300

    :wq

---
+++ python/Desafio_python/Edabit/Edab1.py
@@ -0,0 +1,15 @@
+# Introducao de desafios do site Edabit modo muito facil para iniciantes em Python.
+def hello():
+  return "hello edabit.com" #<-- Returno cria um retorno da função, ou seja, quando a função for chamada, ela vai retornar o valor que está depois do return.
+
+#Errei feio pois me esqueci como escrever return direito.
+def cubes(a):
+	return a ** 3
+
+#Como tonar qualquer resultado numeral escrito em string, em numeros inteiros.
+def string_int(txt):
+	return int(txt)
+
+#Emcomtando  o perimentro de um retangulo, que é a soma de todos os lados.
+def find_perimeter(length, width):
+  return (length + width) * 2
