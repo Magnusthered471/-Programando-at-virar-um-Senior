995d2a7174e0485dbb7445bfd0456107174d8af4
magnusthered471 <ainzooalgown471@gmail.com>
2026-07-10 18:15:31 -0300

    :wq

--- python/Desafio_python/Edabit/Edabit3.py
+++ python/Desafio_python/Edabit/Edabit3.py
@@ -2,7 +2,7 @@
 def football_points(wins, draws, losses):
   return wins * 3 + draws * 1 + losses * 0
 
-# Conversão de string para inteiro e vice-versa
+# Conversão de string para inteiro e vice-vers
 def to_int(txt):
 	return int(txt)
 def to_str(num):
