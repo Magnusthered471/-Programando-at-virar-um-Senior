a22fdc88289d731f82a08df403407e3c75a08eeb
magnusthered471 <ainzooalgown471@gmail.com>
2026-07-22 00:07:58 -0300

    :wq

--- python/Gustavo-Guanabara/011.py
+++ python/Gustavo-Guanabara/011.py
@@ -1,1 +1,1 @@
-# Crie um programa que leia a largura e a altura
+# Crie um programa que leia a largura e a altura de uma parede em metros, calcule a sua area e a quantidade de tinta nessesaria para pinta-la , sabendo que cada litro de tinta, pinta uma area de 2m
