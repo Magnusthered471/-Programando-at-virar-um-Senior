eb502e02cbb8b23d8ebca3f60accdea6d8d7a0e9
magnusthered471 <ainzooalgown471@gmail.com>
2026-07-23 15:58:31 -0300

    :wq

--- python/Gustavo-Guanabara/014.py
+++ python/Gustavo-Guanabara/014.py
@@ -1,1 +1,3 @@
-p1 = float(input("Informe"))
+f = float(input("Informe a temperatura em C: "))
+c = ((f * 9) / 5) + 32
+print(f"A temperatura de {f}C coresponde a {c}F") 
---
+++ python/Gustavo-Guanabara/015.py
@@ -0,0 +1,4 @@
+c = float(input("Quantos dias o carro alugado?: "))
+k = float(input("quantos kilomentros o carro andou?: "))
+n1 = (c * 120) + (k * 0.35)
+print(f"O custo do uso sera: {n1:.2f}")
